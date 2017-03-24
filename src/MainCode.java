7729feaea1250b086e403d4b938575dce082f655
tsuser <tsuser@sys>
2017-03-24 13:18:58 +0530

    Push6

--- src/MainCode.java
+++ src/MainCode.java
@@ -3,8 +3,8 @@ public class MainCode {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("welcome conflict");
-		System.out.println("what is this");
+		System.out.println("welcome conflict hello");
+
 
 	}
 
